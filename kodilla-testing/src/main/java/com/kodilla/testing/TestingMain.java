fefa61ec8904317a9b1fbb15d76effee57369daa
Your Name <you@example.com>
2019-10-09 17:56:19 +0200

    Exercise 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calc = new Calculator();
+        
+        int sum = calc.add(5, 12);
+        int diff = calc.substract(100, 65);
+        
+        if(sum == (5+12)){
+            System.out.println("add correct");
+        } else {
+            System.out.println("add ERROR");
+        }
+        
+        if(diff == (100-65)){
+            System.out.println("diff correct");
+        } else {
+            System.out.println("diff ERROR");
+        }
     }
 }
