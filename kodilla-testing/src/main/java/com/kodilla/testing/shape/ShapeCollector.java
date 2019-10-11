f7b9383e31fe4f3b721d9a4faffcfcdfaa97e829
Your Name <you@example.com>
2019-10-11 10:18:04 +0200

    Exercise 6.4 completed

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -6,18 +6,34 @@ public class ShapeCollector {
     private ArrayList<Shape> shapeList = new ArrayList<>();
     
     public void addFigure(Shape shape){
+        shapeList.add(shape);
     }
     
     public String removeFigure(Shape shape){
-        return null;
+        String message = "There is no such figure in shape list";
+        
+        if(shapeList.contains(shape)){
+            shapeList.remove(shape);
+            message = "Figure removed";
+        }
+        return message;
     }
     
     public Shape getFigure(int n){
-        return null;
+        if(n < 0 || n >= shapeList.size()){
+            return null;
+        }
+        return shapeList.get(n);
     }
     
     public String showFigures(){
-        return null;
+        StringBuilder sb = new StringBuilder();
+        
+        for(Shape s : shapeList){
+            sb.append(s.getShapeName()).append(" ").append(s.getField()).append(" ");
+        }
+        
+        return sb.toString();
     }
 
     public ArrayList<Shape> getShapeList() {
