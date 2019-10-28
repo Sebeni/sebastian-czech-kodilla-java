1eadb96276cd80a8cba7aaa0b5803be1d253fc03
Your Name <you@example.com>
2019-10-28 18:28:37 +0100

    Streams additional task

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/additional/Main.java
@@ -0,0 +1,63 @@
+package com.kodilla.stream.additional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    
+    static long getCountEmptyStringUsingJava8(List<String> strings){
+        return strings.stream()
+                .filter(s -> s.matches("^$"))
+                .count();
+    }
+
+
+    static long getCountLength3UsingJava8(List<String> strings){
+        return strings.stream()
+                .filter(s -> s.length() == 3)
+                .count();
+    }
+
+    static List<String> deleteEmptyStringsUsingJava8(List<String> strings){
+        return strings.stream()
+                .filter(s -> !(s.matches("^$")))
+                .collect(Collectors.toList());
+    }
+    
+    static String getMergedStringUsingJava8(List<String>strings, String delimiter){
+        return strings.stream()
+                .filter(s -> !(s.matches("^$")))
+                .collect(Collectors.joining(delimiter));
+    }
+    
+    static List<Integer> getSquaresJava8(List<Integer> numbers){
+        return numbers.stream()
+                .distinct()
+                .map(integer -> integer*integer)
+                .collect(Collectors.toList());
+    }
+    
+    static Integer getMaxJava8(List<Integer> numbers){
+        return numbers.stream()
+                .mapToInt(value -> value)
+                .max().orElse(Integer.MAX_VALUE);
+    }
+    
+    static Integer getMinJava8(List<Integer> numbers){
+        return numbers.stream()
+                .mapToInt(value -> value)
+                .min().orElse(Integer.MIN_VALUE);
+    }
+    
+    public static Integer getSumJava8(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(value -> value)
+                .sum();
+    }
+
+    public static Integer getAverageJava8(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .mapToInt(value -> value)
+                .average().orElse(0);
+    }
+}
