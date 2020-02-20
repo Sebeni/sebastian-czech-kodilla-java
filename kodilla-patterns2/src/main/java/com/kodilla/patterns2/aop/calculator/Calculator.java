7b61f6f40524690aef842ca9f8be917366f5c284
Sebastian C <seb.czech@wp.pl>
2020-02-20 15:40:07 +0100

    24.3 before exercise aspect oriented programming

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
@@ -0,0 +1,63 @@
+package com.kodilla.patterns2.aop.calculator;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@EnableAspectJAutoProxy
+public class Calculator {
+
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+    public double sub(double x, double y) {
+        return x - y;
+    }
+
+    public double mul(double x, double y) {
+        return x * y;
+    }
+
+    public double div(double x, double y) {
+        if (y == 0) {
+            throw new ArithmeticException("Divide by zero!");
+        }
+
+        return x / y;
+    }
+
+    public BigDecimal factorial(BigDecimal n) {
+        if (n.equals(BigDecimal.ONE) || n.equals(BigDecimal.ZERO)) {
+            return BigDecimal.ONE;
+        } else if (n.compareTo(BigDecimal.ONE) > 0) {
+            return n.multiply(factorial(n.subtract(BigDecimal.ONE)));
+        } else {
+            throw new ArithmeticException("Factorial argument is negative");
+        }
+    }
+    
+    
+    
+    public BigDecimal factorialWithWhile(BigDecimal n) {
+        if (n.equals(BigDecimal.ONE) || n.equals(BigDecimal.ZERO)) {
+            return BigDecimal.ONE;
+        } else if (n.compareTo(BigDecimal.ONE) > 0) {
+            BigDecimal factorial = n;
+
+            while(n.compareTo(BigDecimal.ONE) > 0) {
+                n = n.subtract(BigDecimal.ONE);
+                factorial = factorial.multiply(n);
+
+            }
+            return factorial;
+        } else {
+            throw new ArithmeticException("Factorial argument is negative");
+        }
+        
+        
+    }
+
+}
